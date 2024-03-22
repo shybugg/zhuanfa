4cdede51ea1ffce91bcdd2ea8ab685eb60b22c80
shybugg <rppprb@163.com>
2024-03-22 17:03:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/lib/hooks/use-track.ts
@@ -0,0 +1,24 @@
+import { useSession } from "next-auth/react";
+
+export function useTrack() {
+  const { data: session } = useSession();
+  return {
+    track: (event: string, data: Record<string, any>) => {
+      if (!umami) {
+        return;
+      }
+      if (session?.user) {
+        umami.track(event, {
+          ...data,
+          userId: session.user.id,
+          userEmail: session.user.email ?? "",
+          userName: session.user.name ?? "",
+        });
+      } else {
+        umami.track(event, {
+          ...data,
+        });
+      }
+    },
+  };
+}
